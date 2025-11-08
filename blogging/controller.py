1add1e03dc39079c2dc30dfb856180e6f48b20b7
Michael Chen <michaelchen446@gmail.com>
2025-11-07 18:22:27 -0800

    Added docstrings to Controller class methods

--- blogging/controller.py
+++ blogging/controller.py
@@ -1,13 +1,26 @@
+"""
+controller.py
+Controller class handles blog and post management.
+"""
+
 from blogging.blog import Blog
 from blogging.post import Post
 from datetime import datetime
 
 class Controller:
+    """Controls login, blog, and post interactions."""
 
-    #Initialization
     def __init__(self):
+        """Initialize controller with no blogs and not logged in."""
         self.logged_in = False
         self.blog = []
         self.current_blog = None
 
-
+    def login(self, user, password):
+        """Logs a user in if credentials match."""
+        if self.logged_in:
+            return False
+        if user == "user" and password == "blogging2025":
+            self.logged_in = True
+            return True
+        return False
