5916c8a73315626325d25c98807ab5d1a3a7ca0d
Michael Chen <michaelchen@w134-87-137-106.wireless.uvic.ca>
2025-11-22 00:15:37 -0800

    Step5: Remove blog post storage (PostDAO is source of truth)

--- blogging/blog.py
+++ blogging/blog.py
@@ -1,58 +1,24 @@
-from blogging import post
-
 class Blog:
+    """
+    Blog domain class for Assignment 4
+    (posts are stored externally via PostDAO).
+    """
 
-    #Initializing blog object attributes
     def __init__(self, id, name, url, email):
         self.id = id
         self.name = name
         self.url = url
         self.email = email
-        self.posts = []
 
-    #Method to add post to list of posts in blog
-    def add_post(self, new_post):
-        self.posts.append(new_post)
-
-    #Method that retrieves a certain code from a blog based on posts code
-    def get_post(self, code):
-        for p in self.posts:
-            if p.code == code:
-                return p
-        return None
-
-    #Removes post from blog based on posts code
-    def remove_post(self, code):
-        for p in self.posts:
-            if p.code == code:
-                self.posts.remove(p)
-                return True
-        return False
-
-    #Returns posts that contain any instance of keyword in title or text
-    def retrieve_post(self, keyword):
-        contains = []
-        for p in self.posts:
-            if keyword.lower() in p.title.lower() or keyword.lower() in p.text.lower():
-                contains.append(p)
-        return contains
-
-    #Sorts posts in newest order
-    def list_posts(self):
-        self.posts.sort(key=lambda p: p.code, reverse=True)
-        return self.posts
-
-    #Output blog
     def __str__(self):
         return f"Blog ID: {self.id}\nName: {self.name}\nURL: {self.url}\nEmail: {self.email}"
 
-    #Tests equality of attributes
     def __eq__(self, other):
-        if isinstance(other, self.__class__):
-            return(self.id == other.id and
-                   self.name == other.name and
-                   self.url == other.url and
-                   self.email == other.email)
-        else:
+        if not isinstance(other, Blog):
             return False
-
+        return (
+            self.id == other.id and
+            self.name == other.name and
+            self.url == other.url and
+            self.email == other.email
+        )
